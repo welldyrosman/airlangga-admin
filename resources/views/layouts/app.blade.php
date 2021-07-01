09ca11c200d80f9198f25dd099a50c8ed9b8478a
Welldy <Welldyrosman@gmail.com>
2021-07-01 12:59:38 +0700

    add highcharts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,10 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="{{asset('vendors/plugins/fontawesome-free/css/all.min.css')}}">
   <link rel="stylesheet" href="{{asset('vendors/plugins/summernote/summernote-bs4.min.css')}}">
+
   <!-- Theme style -->
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.html')
 @section('content')
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <div id="content">
     <div class="content-header">
         <div class="container-fluid">
@@ -23,11 +27,10 @@
                   <div class="small-box bg-info">
                     <div class="inner">
                       <h3>150</h3>
-
-                      <p>New Orders</p>
+                      <p>Travel Booking Baru</p>
                     </div>
                     <div class="icon">
-                      <i class="ion ion-bag"></i>
+                      <i class="fas fa-shopping-bag"></i>
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
@@ -37,12 +40,12 @@
                   <!-- small box -->
                   <div class="small-box bg-success">
                     <div class="inner">
-                      <h3>53<sup style="font-size: 20px">%</sup></h3>
+                      <h3>53</h3>
 
-                      <p>Bounce Rate</p>
+                      <p>Studio Booking Baru</p>
                     </div>
                     <div class="icon">
-                      <i class="ion ion-stats-bars"></i>
+                      <i class="fas fa-camera-retro"></i>
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
@@ -53,11 +56,10 @@
                   <div class="small-box bg-warning">
                     <div class="inner">
                       <h3>44</h3>
-
-                      <p>User Registrations</p>
+                      <p>Pelunasan Travel</p>
                     </div>
                     <div class="icon">
-                      <i class="ion ion-person-add"></i>
+                      <i class="fas fa-file-invoice-dollar"></i>
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
@@ -69,16 +71,98 @@
                     <div class="inner">
                       <h3>65</h3>
 
-                      <p>Unique Visitors</p>
+                      <p>Pelunasan Studio</p>
                     </div>
                     <div class="icon">
-                      <i class="ion ion-pie-graph"></i>
+                      <i class="fas fa-file-invoice-dollar"></i>
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
                 </div>
                 <!-- ./col -->
-              </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <figure class="highcharts-figure">
+                        <div id="containerchart"></div>
+                        <p class="highcharts-description">
+                            Chart showing use of rotated axis labels and data labels. This can be a
+                            way to include more labels in the chart, but note that more labels can
+                            sometimes make charts harder to read.
+                        </p>
+                    </figure>
+                </div>
+            </div>
+
         </div>
     </div>
+    <script>
+        Highcharts.chart('containerchart', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Grafik Travel Per Bulan'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Jumlah Pack Terjual'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: '<b>{point.y:.1f} Pack</b>'
+            },
+            series: [{
+                name: 'Population',
+                data: [
+                    ['Shanghai', 24.2],
+                    ['Beijing', 20.8],
+                    ['Karachi', 14.9],
+                    ['Shenzhen', 13.7],
+                    ['Guangzhou', 13.1],
+                    ['Istanbul', 12.7],
+                    ['Mumbai', 12.4],
+                    ['Moscow', 12.2],
+                    ['São Paulo', 12.0],
+                    ['Delhi', 11.7],
+                    ['Kinshasa', 11.5],
+                    ['Tianjin', 11.2],
+                    ['Lahore', 11.1],
+                    ['Jakarta', 10.6],
+                    ['Dongguan', 10.6],
+                    ['Lagos', 10.6],
+                    ['Bengaluru', 10.3],
+                    ['Seoul', 9.8],
+                    ['Foshan', 9.3],
+                    ['Tokyo', 9.3]
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+    }]
+});
+    </script>
 @endsection
